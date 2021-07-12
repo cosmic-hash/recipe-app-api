bb1319e7e71966c0d95b0206fce1538a0d79f2b0
Sai Gautham <saigautham01@gmail.com>
2021-07-12 23:14:15 +0530

    added manage user endpoint

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, get_user_model
-
+from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
@@ -15,6 +15,17 @@ class UserSerializer(serializers.ModelSerializer):
         """create  a new user with encrypted password and retun it"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """upadate a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """serializer for the user authentication object"""
@@ -25,18 +36,18 @@ class AuthTokenSerializer(serializers.Serializer):
     )
 
     def validate(self, attrs):
-       """validate and authenticate the user""" 
-       email = attrs.get('email')
-       password = attrs.get('password')
+        """validate and authenticate the user"""
+        email = attrs.get('email')
+        password = attrs.get('password')
 
-       user = authenticate(
-           request=self.context.get('request'),
-           username=email,
-           password=password
-       )
-       if not user:
-            msg = _('Unable to authenticate with provided credentials')
-            raise serializers.ValidationError(msg, code='authentication')
+        user = authenticate(
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        if not user:
+                msg = _('Unable to authenticate with provided credentials')
+                raise serializers.ValidationError(msg, code='authentication')
 
-       attrs['user'] = user
-       return attrs
+        attrs['user'] = user
+        return attrs
